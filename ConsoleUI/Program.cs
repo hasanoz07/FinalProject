1e8bf993d6f3d0b53651328100cca9db8a41476e
Hasan ÖZ <2011404009@ogr.mehmetakif.edu.tr>
2021-03-04 16:44:58 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,48 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            /*
+            foreach (var product in productManager.GetAll())
+            {
+                
+                Console.WriteLine("Ürün ismi : " + product.ProductName);
+                Console.WriteLine("Ürün fiyatı : " + product.UnitPrice);
+                Console.WriteLine("Ürün stoğu : " + product.UnitsInStock);
+                Console.WriteLine("------------------------------");
+
+            }
+            */
+            /*
+            foreach (var product in productManager.GetAllByCategory(2))
+            {
+
+                Console.WriteLine("Ürün ismi : " + product.ProductName);
+                Console.WriteLine("Ürün fiyatı : " + product.UnitPrice);
+                Console.WriteLine("Ürün stoğu : " + product.UnitsInStock);
+                Console.WriteLine("------------------------------");
+
+            }
+            */
+            foreach (var product in productManager.GetByUnitPrice(10,25))
+            {
+
+                Console.WriteLine("Ürün ismi : " + product.ProductName);
+                Console.WriteLine("Ürün fiyatı : " + product.UnitPrice);
+                Console.WriteLine("Ürün stoğu : " + product.UnitsInStock);
+                Console.WriteLine("------------------------------");
+
+            }
+
+
+        }
+    }
+}
